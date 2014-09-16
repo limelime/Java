e5a32b012d8962f6ab5980c30e37885dfc26f9aa
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-09-16 10:26:27 -0400

    Add listFiles().

--- Java/src/net/xngo/utils/java/io/FileUtils.java
+++ Java/src/net/xngo/utils/java/io/FileUtils.java
@@ -2,8 +2,13 @@ package net.xngo.utils.java.io;
 
 import java.text.DecimalFormat;
 import java.io.File;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+
+import org.apache.commons.io.filefilter.TrueFileFilter;
 
 public class FileUtils
 {
@@ -43,4 +48,45 @@ public class FileUtils
     return size;
   }
 
+  /**
+   * Get a list of unique files from a set of paths(files or directories).
+   * @param paths
+   * @return list of unique files
+   */
+  public static Set<File> listFiles(Set<File> paths)
+  {
+    Set<File> listOfAllUniqueFiles = new HashSet<File>();
+    
+    for(File path: paths)
+    {
+      if(path.exists())
+      {
+        try
+        {
+          File canonicalFilePath = path.getCanonicalFile(); // Get all input files/directories paths as canonical to ensure that there will be no duplicate.
+          if(canonicalFilePath.isFile())
+          {
+            listOfAllUniqueFiles.add(canonicalFilePath);
+          }
+          else
+          {// It is a directory.
+            Collection<File> filesList = org.apache.commons.io.FileUtils.listFiles(canonicalFilePath, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
+            listOfAllUniqueFiles.addAll(filesList);
+          }
+        }
+        catch(IOException e)
+        {
+          e.printStackTrace();
+        }
+        
+      }
+      else
+      {
+        System.out.println(String.format("[Warning] -> [%s] doesn't exist.", path.getAbsolutePath()));
+      }
+    }
+    
+    return listOfAllUniqueFiles;
+  }
+  
 }
