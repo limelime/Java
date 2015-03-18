2e73b043e8b2859a94a4c29765aa6bfc452b80f5
limelime <limelime@users.noreply.github.com>
2015-03-18 15:10:05 -0400

    Add benchmark to find optimal buffer size.

---
+++ Java/Benchmarks/BufferSizeBenchmark.sh
@@ -0,0 +1,4 @@
+java -jar BufferSizeBenchmark.jar "/x/audio/music/club/Energy 2000 - Christmas Party (26.12.10)[mp3@128kbps}.mp3" 1048576 >> BufferSizeBenchmark.log
+# 1048576
+# 8192
+reboot
---
+++ Java/src/net/xngo/utils/bench/java/math/BufferSizeBenchmark.java
@@ -0,0 +1,23 @@
+package net.xngo.utils.bench.java.math;
+
+import java.io.File;
+
+import net.xngo.utils.java.math.Hash;
+
+public class BufferSizeBenchmark
+{
+
+  public static void main(String[] args)
+  {
+    File file = new File(args[0]);
+    int bufferSize = Integer.parseInt(args[1]);
+    
+    long start = System.currentTimeMillis();
+    String hash = Hash.xxhash32(file, bufferSize);
+    long end = System.currentTimeMillis();
+    long runtime = end-start;
+
+    System.out.println(String.format("%,d B | %,d ms| %,d B | %s", bufferSize, runtime, file.length(), file.getAbsolutePath()));
+  }
+
+}
