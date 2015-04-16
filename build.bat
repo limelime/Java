300f4168fdb48b9e8d9aea4731e10a8b4d7c9e6b
limelime <limelime@users.noreply.github.com>
2015-04-16 11:48:57 -0400

    Fix FilesHub path.

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 SET NET_XNGO_HOME=%~dp0
 CALL ant -f %NET_XNGO_HOME%\build.xml
 
-cp %NET_XNGO_HOME%\releases\net.xngo.utils.jar %NET_XNGO_HOME%\..\..\FilesHub\FilesHub\lib
+cp %NET_XNGO_HOME%\releases\net.xngo.utils.jar %NET_XNGO_HOME%\..\FilesHub\releases\latest\libs\
 
 pause
